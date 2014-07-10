957bdd2f376f677a0de775df78850d03a6c4e1a0
Clemenshemmerling <clemens@galileo.edu>
2014-07-10 15:08:24 -0600

    cambio en mapa

--- geo.js
+++ geo.js
@@ -1,4 +1,5 @@
 var geo = navigator.geolocation;
+var opciones = {}
 
 function geo_error(){
 	console.log('no puedo saber donde estas');
@@ -8,7 +9,7 @@ function geo_exito(posicion) {
 	var lat = posicion.coords.latitude;
 	var lon = posicion.coords.longitude;
 	var mapa = new Image();
-	mapa.src = "http://maps.googleapis.com/maps/api/staticmap?zoom=13&size=200x200&sensor=false&center="+lat+","+lon;
+	mapa.src = "http://maps.googleapis.com/maps/api/staticmap?zoom=13&size=300x300&sensor=false&center="+lat+","+lon;
 	$('#geo').append(mapa);
 }
 
