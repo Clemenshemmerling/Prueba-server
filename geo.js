703d2f370e0fd32aff4c25eca475ab8044e200d8
Clemenshemmerling <clemens@galileo.edu>
2014-07-10 18:08:32 -0600

    correcion en geo

--- geo.js
+++ geo.js
@@ -13,7 +13,7 @@ $(function(){
 		mapa.src = "http://maps.googleapis.com/maps/api/staticmap?maptype=hybrid&zoom=13&size=300x300&sensor=false&center="+lat+","+lon;
 		$('#geo').append(mapa);
 
-		window.lat = lat; window.lon = lon;
+		obtenerGeoInformacion(lat, lon)
 }
 
 	geo.getCurrentPosition(geo_exito, geo_error, opciones);
